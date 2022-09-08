2e6c6347e755b4aad111b0f17fa7df22ea107252
Amir Gambo Ibrahim <gambo.amir@yahoo.com>
2022-09-08 13:18:36 -0700

    Printing the sentence "Programming is like building a multilingual puzzle", followed by a new line.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/**
+ *main -print the string in the put function
+ *
+ * Description: using the main function
+ * the program outputs "Programming is like building a multilingual puzzle"
+ * return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
