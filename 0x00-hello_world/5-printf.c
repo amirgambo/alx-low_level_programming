d1fcc5255fd4055bfadd9d16ef5c9e5da1838beb
Amir Gambo Ibrahim <gambo.amir@yahoo.com>
2022-09-08 15:12:17 -0700

    Printing the sentence "with proper grammar, but the oucome is a piece of art,", followed by a new line.

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main-program entry point
+ *
+ * Return: 0 if there is no error
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
