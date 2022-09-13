893b937ef4832fdbc9d96fdffa69bd96f5f1adfb
Amir Gambo Ibrahim <gambo.amir@yahoo.com>
2022-09-13 13:44:46 -0700

    A function that adds two integers and returns the result.

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,14 +3,20 @@
  * _isalpha - function that checks for alphabetic character.
  * @c: the int character as function argument.
  *
- * Return : 0 as always(success)
+ * Return : i if true and 0 otherwise.
  */
 int _isalpha(int c)
 {
-		if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		{
-			return (1);
-		}
-		else
+	if(c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
 		return (0);
+	}
 }
