551d8d197ace562c0a54847e93d869130325dcdb
Amir Gambo Ibrahim <gambo.amir@yahoo.com>
2022-09-15 06:48:51 -0700

    Corrected main.c file

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,17 +2,17 @@
 
 /**
  *
- * * main - tests function that prints if integer is positive or negative
+ * main - tests function that prints if integer is positive or negative
  *
- * * Return: 0
+ * Return: 0
  *
- * */
+ */
 
 int main(void)
 {
 	int i;
 
-	i = 98 % 2;
+	i = 0 ;
 	positive_or_negative(i);
 
 	return (0);
