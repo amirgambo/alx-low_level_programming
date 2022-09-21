6d99bff1a6f32c1c3ad422a4c97ab39fe5a66e7c
Amir Gambo Ibrahim <gambo.amir@yahoo.com>
2022-09-21 08:22:45 -0700

    A function that concatenates two strings.

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,13 +2,13 @@
 
 /**
 * _strncat - concatenate two strings
-* @dest: string to added upon 
+* @dest: string to added upon
 * @src: string to be completed at the end of dest
 * @n: int prameter to compare index to
 * Return: returns new string after concatenation
 */
 
-char *_strncat(char * dest, char *src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
 int index = 0, dest_len = 0;
 
