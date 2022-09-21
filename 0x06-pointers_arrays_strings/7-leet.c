6568a360b2149c1535ad0224a51b5f06f7923259
Amir Gambo Ibrahim <gambo.amir@yahoo.com>
2022-09-21 14:53:58 -0700

    A function that encodes a string into 1337.

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -30,9 +30,7 @@ char transform(char x)
 {
 	char mapping_low[8] = {'o', 'l', '\0', 'e', 'a', '\0', '\0', 't'};
 	char mapping_upper[8] = {'O', 'L', '\0', 'E', 'A', '\0', '\0', 'T'};
-
 	int i = 0;
-
 	char replacement = x;
 
 	while (i < 8)
