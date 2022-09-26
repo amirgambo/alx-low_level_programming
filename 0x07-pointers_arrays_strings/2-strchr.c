646a7caff5185f58b17fb3ac015a2b90365dcb9e
Amir Gambo Ibrahim <gambo.amir@yahoo.com>
2022-09-26 13:47:00 -0700

    A function that locates a character in a string.

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
-* _memcpy - copies n bytes from source memory to destination
-*
-* @dest: destination to write to
-* @src: source to copy from
-* @n: number of bytes to write
-*
-* Return: pointer to dest
-*/
+ * _memcpy - copies a memory area
+ * @dest: destination to write to
+ * @src: source to copy from
+ * @n: number of bytes to write
+ *
+ * Return: pointer to dest
+ */
 
 char *_memcpy(char *dest, char *src, int n)
 {
