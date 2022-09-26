4a864eee391657d5680fb818d6691e1e054937b1
Amir Gambo Ibrahim <gambo.amir@yahoo.com>
2022-09-26 13:00:02 -0700

    A function that copies memory area.

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,12 +1,12 @@
-/** 
- * _memset - sets a given amount of bytes to a value at an address 
- * 
- * @s: memory location to start at 
- * @b: character to write to memory 
- * @n: number of bytes to write
- * 
- * Return: pointer to s 
- */
+/**
+* _memset - sets a given amount of bytes to
+* a value at an address
+* @s: memory location to start at
+* @b: character to write to memory
+* @n: number of bytes to write
+*
+* Return: pointer to s
+*/
 
 char *_memset(char *s, char b, unsigned int n)
 {
@@ -17,4 +17,3 @@ char *_memset(char *s, char b, unsigned int n)
 
 	return (s);
 }
-
