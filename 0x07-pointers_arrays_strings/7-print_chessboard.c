71184f2a481015904eb16318cebe4d471f5035d0
Amir Gambo Ibrahim <gambo.amir@yahoo.com>
2022-09-27 08:52:37 -0700

    A function that prints the chessboard.

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -14,7 +14,6 @@ void print_chessboard(char (*a)[8])
 
 	i = 0;
 	j = 0;
-
 	while (i < 64)
 	{
 		if (i % 8 == 0 && i != 0)
@@ -22,7 +21,6 @@ void print_chessboard(char (*a)[8])
 			j = i;
 			_putchar('\n');
 		}
-
 		_putchar(a[i / 8][i - j]);
 		i++;
 	}
