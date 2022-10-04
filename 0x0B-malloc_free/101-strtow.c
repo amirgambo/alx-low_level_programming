aa08623a8f46b7cf4c033cd3513ffe9a69aff496
Amir Gambo Ibrahim <gambo.amir@yahoo.com>
2022-10-04 04:47:30 -0700

    A function that splits a string into words.

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -15,7 +15,6 @@ void ch_free_grid(char **grid, unsigned int height)
 	{
 		for (; height > 0; height--)
 			free(grid[height]);
-
 		free(grid[height]);
 		free(grid);
 	}
@@ -35,30 +34,24 @@ char **strtow(char *str)
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
-
 	for (c = height = 0; str[c] != '\0'; c++)
 		if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
 			height++;
-
 	aout = malloc((height + 1) * sizeof(char *));
-
 	if (aout == NULL || height == 0)
 	{
 		free(aout);
 		return (NULL);
 	}
-
 	for (i = a1 = 0; i < height; i++)
 	{
 		for (c = a1; str[c] != '\0'; c++)
 		{
 			if (str[c] == ' ')
 				a1++;
-
 			if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
 			{
 				aout[i] = malloc((c - a1 + 2) * sizeof(char));
-
 				if (aout[i] == NULL)
 				{
 					ch_free_grid(aout, i);
