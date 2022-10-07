c1deacd078ee62c03d41db65d9d5a87818be1377
Amir Gambo Ibrahim <gambo.amir@yahoo.com>
2022-10-07 09:54:28 -0700

    A program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -14,7 +14,6 @@ void add_nums(char *final_prod, char *next_prod, int next_len);
 *
 * Return: The length of the string.
 */
-
 int find_len(char *str)
 {
 	int len = 0;
@@ -34,7 +33,6 @@ int find_len(char *str)
 * function exits with a status of 98.
 * Return: A pointer to the array.
 */
-
 char *create_xarray(int size)
 {
 	char *array;
@@ -60,7 +58,6 @@ char *create_xarray(int size)
 *
 * Return: A pointer to the next non-zero element.
 */
-
 char *iterate_zeroes(char *str)
 {
 	while (*str && *str == '0')
@@ -77,7 +74,6 @@ char *iterate_zeroes(char *str)
 * exits with a status of 98.
 * Return: The converted int.
 */
-
 int get_digit(char c)
 {
 	int digit = c - '0';
@@ -101,7 +97,6 @@ int get_digit(char c)
 * Description: If mult contains a non-digit, the function
 * exits with a status value of 98.
 */
-
 void get_prod(char *prod, char *mult, int digit, int zeroes)
 {
 	int mult_len, num, tens = 0;
@@ -147,7 +142,6 @@ void get_prod(char *prod, char *mult, int digit, int zeroes)
 * @next_prod: The next product to be added.
 * @next_len: The length of next_prod.
 */
-
 void add_nums(char *final_prod, char *next_prod, int next_len)
 {
 	int num, tens = 0;
@@ -193,7 +187,6 @@ void add_nums(char *final_prod, char *next_prod, int next_len)
 * contains non-digits, the function exits with a status of 98.
 * Return: Always 0.
 */
-
 int main(int argc, char *argv[])
 {
 	char *final_prod, *next_prod;
