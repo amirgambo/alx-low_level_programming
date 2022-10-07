5eccc337a74a20734e3a6fe9a0d96d96a30b3fab
Amir Gambo Ibrahim <gambo.amir@yahoo.com>
2022-10-07 06:51:11 -0700

    A program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,8 +9,8 @@
 
 int main(int argc, char *argv[])
 {
-	unsigned long mul;
-	int i, j;
+unsigned long mul;
+int i, j;
 	if (argc != 3)
 	{
 		printf("Error\n");
@@ -30,5 +30,5 @@ int main(int argc, char *argv[])
 	}
 	mul = atol(argv[1]) * atol(argv[2]);
 	printf("%lu\n", mul);
-	return (0);
+return (0);
 }
