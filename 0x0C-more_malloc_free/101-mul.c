339aee81573472d43b33bcf592e1edc7d5e90ac7
Amir Gambo Ibrahim <gambo.amir@yahoo.com>
2022-10-07 09:42:51 -0700

    A program that multiplies two positive numbers.

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -27,11 +27,11 @@ int find_len(char *str)
 
 /**
 * create_xarray - Creates an array of chars and initializes it with
-*                 the character 'x'. Adds a terminating null byte.
+* the character 'x'. Adds a terminating null byte.
 * @size: The size of the array to be initialized.
 *
 * Description: If there is insufficient space, the
-*              function exits with a status of 98.
+* function exits with a status of 98.
 * Return: A pointer to the array.
 */
 
@@ -55,7 +55,7 @@ char *create_xarray(int size)
 
 /**
 * iterate_zeroes - Iterates through a string of numbers containing
-*                  leading zeroes until it hits a non-zero number.
+* leading zeroes until it hits a non-zero number.
 * @str: The string of numbers to be iterate through.
 *
 * Return: A pointer to the next non-zero element.
@@ -74,7 +74,7 @@ char *iterate_zeroes(char *str)
 * @c: The character to be converted.
 *
 * Description: If c is a non-digit, the function
-*              exits with a status of 98.
+* exits with a status of 98.
 * Return: The converted int.
 */
 
@@ -99,7 +99,7 @@ int get_digit(char c)
 * @zeroes: The necessary number of leading zeroes.
 *
 * Description: If mult contains a non-digit, the function
-*              exits with a status value of 98.
+* exits with a status value of 98.
 */
 
 void get_prod(char *prod, char *mult, int digit, int zeroes)
@@ -190,7 +190,7 @@ void add_nums(char *final_prod, char *next_prod, int next_len)
 * @argc: An array of pointers to the arguments.
 *
 * Description: If the number of arguments is incorrect or one number
-*              contains non-digits, the function exits with a status of 98.
+* contains non-digits, the function exits with a status of 98.
 * Return: Always 0.
 */
 
