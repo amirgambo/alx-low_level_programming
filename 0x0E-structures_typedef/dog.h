6b5f0ae189a60966b156fdc0c70fdec304f254c4
Amir Gambo Ibrahim <gambo.amir@yahoo.com>
2022-10-10 11:11:46 -0700

    A new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,18 +10,15 @@
 struct dog
 {
 	char *name;
-	char *owner;
 	float age;
+	char *owner;
 };
-
 /**
- * dog_t - typedef for dog
- */
-
+* dog_t - typedef for dog
+*/
 typedef struct dog dog_t;
 void free_dog(dog_t *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
 #endif
